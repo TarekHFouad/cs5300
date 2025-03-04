d34626224728269a13034da45128a0b21405b2cd
Tarek Fouad <tahmedfo@uccs.edu>
2025-03-04 04:57:51 +0000

    Push to test

--- HW2/bookings/urls.py
+++ HW2/bookings/urls.py
@@ -10,8 +10,8 @@ router.register(r'api/bookings', BookingViewSet)
 
 urlpatterns = [
     # API endpoints
-    path('', include(router.urls)),
-    # Template views
+    path('', movie_list, name='movie_list'),
+    path('api/', include(router.urls)),
     path('movies/', movie_list, name='movie_list'),
     path('book_seat/<int:movie_id>/', seat_booking, name='book_seat'),
     path('booking_history/', booking_history, name='booking_history'),
